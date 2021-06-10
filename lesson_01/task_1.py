695ea8766a7b6754760c97216dbac451b4959ce9
elron <p_a_u_l2003@rambler.ru>
2021-06-11 00:38:57 +0300

    add lesson_01

---
+++ lesson_01/task_1.py
@@ -0,0 +1,21 @@
+"""
+1. Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк и сохраните в переменные, выведите на экран.
+"""
+
+a = 1
+b = 12.34
+c = True
+d = 'text1'
+e = "text2"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+name = input("What is your name? ")
+print(f"Hello {name}!")
+age = int(input("What is your age? "))
+print(f"Your age is {age}")
+print("end")
